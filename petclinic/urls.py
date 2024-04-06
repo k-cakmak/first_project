2bc026259216e507aa16530a067668cc11cf5d4a
kcakmak <kamil.cakmak@bilgeadam.com>
2024-04-06 14:15:51 +0300

    model interaction, template engine

--- petclinic/urls.py
+++ petclinic/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('home/', views.index_page, name="home"),
+    path('owners/', views.list_owner, name="owners"),
 ]
--- petclinic/views.py
+++ petclinic/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Owner
+
 # from django.template import loader
 # from django.http import HttpResponse
 
@@ -9,3 +11,8 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, template_name="index.html", context={})
+
+def list_owner(request):
+    all_objects = Owner.objects.all()
+    data = {"objects":all_objects}
+    return render(request, template_name="owner_list.html", context=data)
