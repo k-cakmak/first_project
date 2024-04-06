f2729867b18ba7086cec19070cf34cfc7d114080
kcakmak <kamil.cakmak@bilgeadam.com>
2024-04-06 12:51:47 +0300

    templete rendering

--- petclinic/views.py
+++ petclinic/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+# from django.template import loader
+# from django.http import HttpResponse
 
 # Create your views here.
+# def index_page(request):
+#     template = loader.get_template('index.html')
+#     return HttpResponse(template.render())
+
+def index_page(request):
+    return render(request, template_name="index.html", context={})
