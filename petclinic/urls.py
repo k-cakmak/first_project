8e465b1faea15c5d9a3d529f2a011ebea8a84568
kcakmak <kamil.cakmak@bilgeadam.com>
2024-04-20 14:03:13 +0300

    feat: class based views are implemented

--- petclinic/urls.py
+++ petclinic/urls.py
@@ -3,9 +3,16 @@ from . import views
 
 urlpatterns = [
     path('home/', views.index_page, name="home"),
-    path('owners/', views.list_owner, name="owners"),
-    path('owner/<int:pk>', views.detail_owner, name="detail-owner"),
-    path('owner/', views.create_owner, name="owner"),
-    path('owner/update/<int:pk>', views.update_owner, name="updateowner"),
-    path('owner/delete/<int:pk>', views.delete_owner, name="delete-owner"),
+    #path('owners/', views.list_owner, name="owners"),
+    path('owners/', views.OwnerListView.as_view(), name="owner-list"),
+    #path('owner/<int:pk>', views.detail_owner, name="detail-owner"),
+    path('owner/<int:pk>', views.OwnerDetailView.as_view(), name="detail-owner"),
+    #path('owner/', views.create_owner, name="owner"),
+    path('owner/', views.OwnerCreateView.as_view(), name="owner-create"),
+    # path('owner/update/<int:pk>', views.update_owner, name="updateowner"),
+    path('owner/update/<int:pk>', views.OwnerUpdateView.as_view(), name="update-owner"),
+    #path('owner/delete/<int:pk>', views.delete_owner, name="delete-owner"),
+    path('owner/delete/<int:pk>', views.OwnerDeleteView.as_view(), name="delete-owner"),
+    path('pet/', views.PetCreateView.as_view(), name="pet-create"),
+    path('pet/<int:pk>', views.PetDetailView.as_view(), name="detail-pet"),
 ]
--- petclinic/views.py
+++ petclinic/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
-from .models import Owner
-from .forms import OwnerForm
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+#from django.views.generic.detail import DetailView
+from .models import Owner, Pet
+#from .forms import OwnerForm
 
 # from django.template import loader
 # from django.http import HttpResponse
@@ -14,48 +18,81 @@ from .forms import OwnerForm
 def index_page(request):
     return render(request, template_name="index.html", context={})
 
-def list_owner(request):
-    all_objects = Owner.objects.all()
-    data = {"objects":all_objects}
-    return render(request, template_name="petclinic/owner_list.html", context=data)
+# def list_owner(request):
+#     all_objects = Owner.objects.all()
+#     data = {"object_list":all_objects}
+#     return render(request, template_name="petclinic/owner_list.html", context=data)
 
-def detail_owner(request, pk):
-    obj = Owner.objects.get(pk=pk)
-    pets = obj.pet_set.all()
-    return render(request, "petclinic/owner.html", context={"owner":obj, "pets":pets})
+class OwnerListView(ListView):
+    model = Owner
+    #ordering = ("name", "last_name")
+    ordering = "-name"
 
-def create_owner(request):
-    if request.method == 'POST':
-        form = OwnerForm(request.POST)
-        if form.is_valid():
-            # obj = Owner()
-            # obj.name = form.cleaned_data["name"]
-            # obj.last_name = form.cleaned_data["last_name"]
-            # obj.email = form.cleaned_data["email"]
-            # obj.phone = form.cleaned_data["phone"]
-            # obj.save()
-            # alternatif yöntem
-            #Owner.objects.create(**form.cleaned_data)
-            # eğer model form kullanıldıysa 
-            form.save()
-            return HttpResponseRedirect("/owners")
-    form = OwnerForm()
-    return render(request, "petclinic/owner_form.html", context={"form":form})
+# def detail_owner(request, pk):
+#     obj = Owner.objects.get(pk=pk)
+#     pets = obj.pet_set.all()
+#     return render(request, "petclinic/owner.html", context={"owner":obj, "pets":pets})
 
-def update_owner(request, pk):
-    obj = Owner.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = OwnerForm(request.POST, instance=obj)
-        if form.is_valid():
-            form.save()
-            return redirect("/owners")
-    form = OwnerForm(instance=obj)
-    return render(request, "petclinic/owner_form.html", context={"form":form})
+class OwnerDetailView(DetailView):
+    model = Owner
+    template_name = "petclinic/owner.html"
 
-def delete_owner(request, pk):
-    obj = Owner.objects.get(pk=pk)
-    if request.method == 'POST':
-        obj.delete()
-        return redirect("/owners")
-    return render(request, "petclinic/owner_delete.html", context={"owner":obj})
+# def create_owner(request):
+#     if request.method == 'POST':
+#         form = OwnerForm(request.POST)
+#         if form.is_valid():
+#             # obj = Owner()
+#             # obj.name = form.cleaned_data["name"]
+#             # obj.last_name = form.cleaned_data["last_name"]
+#             # obj.email = form.cleaned_data["email"]
+#             # obj.phone = form.cleaned_data["phone"]
+#             # obj.save()
+#             # alternatif yöntem
+#             #Owner.objects.create(**form.cleaned_data)
+#             # eğer model form kullanıldıysa 
+#             form.save()
+#             return HttpResponseRedirect("/owners")
+#     form = OwnerForm()
+#     return render(request, "petclinic/owner_form.html", context={"form":form})
 
+class OwnerCreateView(CreateView):
+    model = Owner
+    #fields = ["name", "last_name", "email"]
+    fields = "__all__"
+    success_url = "/owners"
+
+# def update_owner(request, pk):
+#     obj = Owner.objects.get(pk=pk)
+#     if request.method == 'POST':
+#         form = OwnerForm(request.POST, instance=obj)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("/owners")
+#     form = OwnerForm(instance=obj)
+#     return render(request, "petclinic/owner_form.html", context={"form":form})
+
+class OwnerUpdateView(UpdateView):
+    model = Owner
+    fields = "__all__"
+    template_name_suffix = "_update_form"
+    success_url = "/owners"
+
+# def delete_owner(request, pk):
+#     obj = Owner.objects.get(pk=pk)
+#     if request.method == 'POST':
+#         obj.delete()
+#         return redirect("/owners")
+#     return render(request, "petclinic/owner_delete.html", context={"owner":obj})
+
+class OwnerDeleteView(DeleteView):
+    model = Owner
+    success_url = "/owners"
+    template_name = "petclinic/owner_delete.html"
+
+class PetCreateView(CreateView):
+    model = Pet
+    fields = "__all__"
+    success_url = "/owners"
+
+class PetDetailView(DetailView):
+    model = Pet
